a463fe0ad670d3b903ac339b5003c97c02db6d7e
www25820 <3209256473@qq.com>
2026-07-13 18:31:41 +0800

    feat: ThreadPool 升级泛型 enqueue — std::function + std::bind 任意任务

--- threatpool.hpp
+++ threatpool.hpp
@@ -5,23 +5,24 @@
 #include<condition_variable>
 #include<string>
 #include <vector>
+#include <functional>//引入function万能容器
 class ThreadPool {
     public:
     ThreadPool(int numThreads):done(false) {
         for(int i=0;i<numThreads;i++){
-            threads.emplace_back(([this]{
+            threads.emplace_back([this]{
                 while(true){
                     std::unique_lock<std::mutex> lock(mtx);
                     cv.wait(lock, [this]{ return !tasks.empty() || done; });
                     if(done && tasks.empty()){
                         break;
                     }
-                    auto task = tasks.front();//等效std::function<void()>task(std::move(tasks.front());//取任务
+                    auto task = std::move(tasks.front());//等效std::function<void()>task(std::move(tasks.front());//取任务
                     tasks.pop();
                     lock.unlock(); // 释放锁，允许其他线程访问队列
-                    std::cout << "消费: " << task << std::endl;
+                    task(); // 执行任务
                 }
-            }));
+            });
         }
         
     }
@@ -37,17 +38,19 @@ class ThreadPool {
             }
         }
     }
-    void addTask(const std::string& task) {
+    template<typename F, typename... Args>
+    void enqueue(F&& f, Args&&... args) {//函数内&&万能引用
+        std::function<void()> task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
         {
             std::lock_guard<std::mutex> lock(mtx);
-            tasks.push(task);
-            std::cout << "生产: " << task << std::endl;
+            tasks.emplace(std::move(task));
         }
+        
         cv.notify_one(); // 通知一个线程有新任务
     }
     private:
         std::vector<std::thread> threads;       // 线程池
-        std::queue<std::string> tasks;       // 任务队列
+        std::queue<std::function<void()>> tasks;       // 任务队列
         std::condition_variable cv;          // 条件变量：通知"有活了"
         std::mutex mtx;                      // 保护队列 + 条件变量
         bool done = false;                   // 生产结束标志（消费者据此退出）
